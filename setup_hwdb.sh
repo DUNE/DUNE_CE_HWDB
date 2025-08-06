291775a9f1def61f10f0cde22d2d8522b5d97909
Martin Tzanov <mtzanov@lsu.edu>
2025-08-06 12:58:34 -0500

    adding site location variable

--- setup_hwdb.sh
+++ setup_hwdb.sh
@@ -2,5 +2,5 @@ htgettoken --vaultserver=htvaultprod.fnal.gov --issuer=fermilab
 
 export TOKENLOC='/run/user/1000/bt_u1000'
 export HWDBSELECT='DEV'
-export COMMANDVERB='VERB0'
+export COMMANDVERB='VERB1'
 export SITELOC='LSU'
